e8196a5305627517037e371db6282481642a8ac7
rahul <rahulsharmacs50@gmail.com>
2018-10-17 14:57:51 +0530

    blink detector and counter complete

--- 9. Blink Detection/blink_detector.py
+++ 9. Blink Detection/blink_detector.py
@@ -38,7 +38,7 @@ def eye_aspect_ratio(eye):
 
 #construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-detector", required = True, help = "path to facial landmark predictor")
+ap.add_argument("-p", "--shape-predictor", required = True, help = "path to facial landmark predictor")
 ap.add_argument("-v", "--video", type = str, default = "", help = "path to input video file")
 
 args = vars(ap.parse_args())
@@ -58,3 +58,100 @@ EYE_AR_CONSEC_FRAMES = 3
 
 COUNTER = 0
 TOTAL = 0
+
+#initialize dlib's face detector (HOG-based) and then create
+#facial landmark detector
+
+print("[INFO] loading facial landmark predictor")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+#grab the indexes of the facial landmarks for the left and 
+#right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+#start the video stream thread
+print("[INFO] starting the bideo stream thread...")
+vs = FileVideoStream(args["video"]).start()
+fileStream = True
+vs = VideoStream(src = 0).start()
+#vs = VideoStream(usePiCamera = True).start()
+fileStream = False;
+time.sleep(1.0)
+
+#loop over the frames from the video stream
+while True:
+	#if this is a video file streamer, then we need to check
+	#if there are any more frames left in the buffer to process
+	if fileStream and not vs.more():
+		break
+
+	#grab the frame from the threaded video file stream, resize
+	#it, and convert it to grayscale
+	frame = vs.read()
+	frame = imutils.resize(frame, width = 450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	#detect faces in the grayscale frame
+	rects = detector(gray, 0)
+
+	#loop over the face detections
+	for rect in rects:
+		#determine the facial landmarks for the face region, then
+		#convert the facial landmark (x, y)- coordinates to a NumPy
+		#array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+
+		#extract the left and right eye coordinates, then use the
+		#coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+
+		#average the eye aspect ration together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
+
+		#compute the convex hull for the left and right eye, then
+		#visualize each of the eyes
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+		#check to see if the eye aspect ratio is below the blink
+		#threshold, and if so, increment the blink frame counter
+
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
+
+		#otherwise, the eye aspect ratio is not beliw the blink threshold
+		else:
+			#if the eyes were closed for a sufficient number of frames
+			#then increment the total number of blinks
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				TOTAL += 1
+			#reset the eye frame counter
+
+			COUNTER = 0
+
+		#draw the total number of blinks on the frame along with
+		#the computed eye aspect ratio for the frame
+
+		cv2.putText(frame, "Blinks:{}".format(TOTAL), (10, 30), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "EAR:{:.2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
+
+	#show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	#if the 'q' key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+#do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
